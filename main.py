65fd51fa422b5edad60d7524ee83a5cff9fd62e0
chonlatee11 <46771457+chonlatee11@users.noreply.github.com>
2022-06-01 16:28:17 +0700

    asian

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 years = [str(1960 + i) for i in range(61)]
-countries = ["United States" , "Thailand" , "Spain" , "Canada" , "Austria" , "Italy" , "Germany"]
+countries = ["Myanmar" , "Thailand" , "Malaysia" , "Indonesia" , "Philippines" , "Singapore" , "Vietnam" , "Lao PDR" , "Cambodia" , "Brunei Darussalam"]
 df = pd.read_csv('gdp.csv')
 
 df = df.query("`Country Name` == @countries")
@@ -17,7 +17,7 @@ countries = df['Country Name']
 
 df = df[["Country Name"] + years].dropna(axis=1)
 
-data = np.empty((1 , 7))
+data = np.empty((1 , 10))
 
 for year in df.columns[1:]:
     data = np.append(data , [df[year].to_numpy()] , 0)
@@ -32,7 +32,7 @@ for i in range(1 , len(data)):
         plot_steps = np.append(plot_steps , [last_gdp + differences * j] , 0)
     last_gdp = data[i]
 
-colors = ["#ff8888" , "#ff22dd" , "#5555ff" , "#55ccff" , "#ffaa55" , "#ff55ff" , "#ffdd22"]
+colors = ["#ff8888" , "#ff22dd" , "#5555ff" , "#55ccff" , "#ffaa55" , "#ff55ff" , "#ffdd22" , "#ff5622" ,"#2211dd" , "#334411"]
 years = iter(df.columns[1:])
 for i , step in enumerate(plot_steps):
     if i % 100 == 0:
@@ -51,10 +51,11 @@ for i , step in enumerate(plot_steps):
     ordered_names = [country_names[i] for i in list_index]
     ordered_colors = [colors[i] for i in list_index]
     
-    plt.title("GDP Per Capita")
+    plt.title("GDP Per Capita Asian")
     plt.barh(ordered_names[::-1] , list_sorted , color = ordered_colors[::-1])
     plt.text(50000 , 0.2 , year , fontsize = 18)
-    plt.xlim(0 , 70000)
+    plt.xlim(0 , 120000)
     plt.pause(0.001)
     plt.clf()
 plt.show()
+
